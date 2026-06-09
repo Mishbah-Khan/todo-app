dd5fc8afd34fc02c898d3efc32e9b68d57efe286
Mishbah Khan <mishbahkhan505@gmail.com>
2026-06-10 02:11:29 +0600

    add success , error message and create task

--- app.js
+++ app.js
@@ -1,21 +1,60 @@
 const API_URL = 'https://6a170e181b90031f81b1f509.mockapi.io/api/pro/v1/todo';
 
-const addText = document.getElementById('todoInput').value;
+// Global variables
+const todoInput = document.getElementById('todoInput');
 const addBtn = document.getElementById('addTodoBtn');
 const todoList = document.getElementById('todoList');
+const loading = document.getElementById('loading');
+const error = document.getElementById('error');
+const success = document.getElementById('success');
+const noTask = document.getElementById('no-task');
 
 
+// Error function
+function errerMessage (errorText){
+    error.innerText = errorText;
+    error.style.display = 'block';
+    setTimeout(
+        ()=>{
+            error.style.display = 'none';
+        }
+    , 3000)
+}
+// success text
+function successMessage (successText){
+    success.innerText = successText;
+    success.style.display = 'block';
+    setTimeout(
+        ()=>{
+            success.style.display = 'none';
+        }
+    , 3000)
+}
 
-
+// Get task list
 async function fetchTaskList() {
+    loading.style.display = 'block';
+   try {
     const fetchData = await fetch(API_URL)
     const data = await fetchData.json();
 
+    if (data.length === 0) {
+        noTask.style.display = 'block';
+    }
     data.forEach((task) => {
         createElement(task)
     });
+
+    loading.style.display = 'none';
+
+   } catch (error) {
+    loading.style.display = 'none';
+    errerMessage('Failed to load task.')
+    
+   }
 }
 
+// update check box 
 async function checkUpdate(id, taskValidation){
     const fetchData = await fetch(`${API_URL}/${id}`, {
         method: "PUT",
@@ -23,6 +62,7 @@ async function checkUpdate(id, taskValidation){
         body: JSON.stringify({isCompleted: taskValidation})
     }); 
 }
+
 
 function createElement(task){
 const li = document.createElement('li');
@@ -36,8 +76,7 @@ li.innerHTML =`
         <button class="save-btn">Save</button>
         <button class="cancel-btn">Cancel</button>
         <button class="delete-btn">Delete</button>
-    </div>
-`;
+    </div>`;
 
 if (task.isCompleted) {
     li.classList.add('completed');
@@ -53,16 +92,49 @@ checkBox.addEventListener('change', async function(){
     if (this.checked) {
         li.classList.add('completed');
         btns.forEach(btn => btn.disabled = true);
-        await checkUpdate(id, true);        
+        await checkUpdate(id, true);
+
     } else {
         li.classList.remove('completed');
         btns.forEach(btn => btn.disabled = false);
         await checkUpdate(id, false);
     }
 });
+}
 
+// Create task 
+async function addTaskToList(){
+    const title = todoInput.value;
+    if (!title) {
+        errerMessage('Please enter task');
+        return;
+    }
+    const data = {
+        title
+    }
+    try {
+        const res = await fetch(API_URL, {
+            method: 'POST',
+            headers: {'content-type': 'application/json'},
+            body: JSON.stringify(data)
+        });
+
+        if (res.ok) {
+            console.log(res);
+            todoList.innerHTML = "";
+            fetchTaskList();
+            successMessage('Task added successfully');
+            todoInput.value = '';
+        }
+        
+    } catch (error) {
+        loading.style.display = 'none';
+        errerMessage('Failed to create task.');
+        console.log(error);
+        
+    }
+    
 }
 
 fetchTaskList();
-
-
+addBtn.addEventListener('click', ()=>{addTaskToList()});
